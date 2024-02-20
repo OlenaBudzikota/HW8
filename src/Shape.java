6228a692dec08601a0ec2d388eb3286b7e1bb247
OlenaBudzikota <budzikovaolena@gmail.com>
2024-02-20 18:31:44 +0200

    Initial commit

---
+++ src/Shape.java
@@ -0,0 +1,56 @@
+interface Shape {
+    void shapeName();
+}
+
+class Circle implements Shape{
+    @Override
+    public void shapeName() {
+        System.out.println("Circle");
+    }
+}
+
+class Triangle implements Shape{
+    @Override
+    public void shapeName(){
+        System.out.println("Triangle");
+    }
+}
+class Cube implements Shape{
+    @Override
+    public void shapeName() {
+        System.out.println("Cube");
+    }
+}
+
+class Rectangle implements Shape{
+    @Override
+    public void shapeName() {
+        System.out.println("Rectangle");
+    }
+}
+class Star implements Shape{
+
+    @Override
+    public void shapeName() {
+        System.out.println("Star");
+    }
+}
+
+
+class Print{
+    public void nameShape(Shape shape){
+
+        shape.shapeName();
+    }
+}
+
+class PrintOut{
+    public static void main(String[] args) {
+
+        new Print().nameShape(new Star());
+        new Print().nameShape(new Cube());
+        new Print().nameShape(new Rectangle());
+        new Print().nameShape(new Triangle());
+
+    }
+}
